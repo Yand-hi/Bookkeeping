0f06f6221c494c7654fd933d5cbc517564d41d67
Yandhi <15877410869@163.com>
2022-02-20 03:18:05 +0800

    配置通过目录引入 svg icons

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-type Icon = {
-  name: String
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {
+  importAll(require.context('../assets/icons', true, /\.svg$/));
+} catch (error) {
+  console.log(error);
 }
-const Icon = (props: Icon) => {
+
+type Props = {
+  name: string
+}
+
+const Icon = (props: Props) => {
   return (
     <svg className="icon">
       <use xlinkHref={'#' + props.name}/>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,10 +3,6 @@ import {Link} from 'react-router-dom'
 import React from 'react'
 import Icon from './Icon'
 
-require('../icons/money.svg')
-require('../icons/chart.svg')
-require('../icons/tag.svg')
-
 const NavWrapper = styled.nav`
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   line-height: 24px;
@@ -35,15 +31,15 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <Icon name={'money'}/>
+          <Icon name="money"/>
           <Link to="/">记一笔</Link>
         </li>
         <li>
-          <Icon name={'tag'}/>
+          <Icon name="tag"/>
           <Link to="/tags">标签</Link>
         </li>
         <li>
-          <Icon name={'chart'}/>
+          <Icon name="chart"/>
           <Link to="/statistics">统计</Link>
         </li>
       </ul>
