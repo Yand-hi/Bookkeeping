718b48242fb0253683eda76048b406772bc22dea
Yandhi <15877410869@163.com>
2022-02-20 03:52:16 +0800

    重构:将所有页面组件封装至 views 目录

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {HashRouter as Router, Switch, Route, Redirect} from "react-router-dom";
-import Layout from './components/Layout'
+import Statistics from './views/Statistics'
+import NoMatch from './views/NoMatch'
+import Money from './views/Money'
+import Tags from './views/Tags'
 
 function App() {
   return (
@@ -26,30 +29,6 @@ function App() {
 
 export default App
 
-function NoMatch() {
-  return <h1>404 NotFound!</h1>
-}
 
-function Money() {
-  return (
-    <Layout>
-      <h2>记一笔页面</h2>
-    </Layout>
-  );
-}
 
-function Tags() {
-  return (
-    <Layout>
-      <h2>标签页面</h2>
-    </Layout>
-  )
-}
 
-function Statistics() {
-  return (
-    <Layout>
-      <h2>统计页面</h2>
-    </Layout>
-  )
-}
