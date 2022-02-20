0179c7716600b63f39ec1144c3578df5b51c2892
Yandhi <15877410869@163.com>
2022-02-21 03:02:06 +0800

    feat: NumberPad component style

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -79,6 +79,40 @@ const CategorySection = styled.section`
   }
 `;
 const NumberPadSection = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  > .output {
+    background: white;
+    font-size: 36px;
+    line-height: 72px;
+    text-align: right;
+    padding: 0 16px;
+    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25),
+    inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
+  }
+
+  > .pad {
+    background: #fafafa;
+
+    > button {
+      background: #fff;
+      font-size: 24px;
+      float: left;
+      width: 25%;
+      height: 64px;
+      border: 4px solid #fafafa;
+
+      &.ok {
+        height: 192px;
+        float: right;
+      }
+
+      &.zero {
+        width: 50%;
+      }
+    }
+  }
 `;
 
 const Money = () => {
@@ -106,22 +140,22 @@ const Money = () => {
         </ul>
       </CategorySection>
       <NumberPadSection>
-        <div>0</div>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>删除</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>清空</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>OK</button>
-        <button>.</button>
-        <button>0</button>
-        <button>.</button>
+        <div className="output">0</div>
+        <div className="pad clearfix">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>-</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button className="ok">确定</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button className="zero">0</button>
+          <button className="dot">.</button>
+        </div>
       </NumberPadSection>
     </Layout>
   );
