c74a52fef0125b4f1fd1c495da975209b89e3ec5
Yandhi <15877410869@163.com>
2022-02-22 02:52:17 +0800

    feat: add tag done

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -3,6 +3,11 @@ import styled from 'styled-components'
 import Icon from '../../components/Icon'
 import {Output} from './Output'
 
+type Tags = {
+  iconName: string,
+  name: string
+}
+
 const Wrapper = styled.section`
   background: white;
   padding: 12px 16px;
@@ -47,19 +52,43 @@ const Wrapper = styled.section`
 `;
 
 const TagsSection: React.FC = () => {
-  const [tags, setTags] = useState<string[]>(
-    ['餐饮', '衣服', '住房', '出行', '酒店', '红包', '转账', '商店', '旅行', '娱乐', '其它', '添加']
-  )
+  const [tags, setTags] = useState<Tags[]>([
+    {iconName: '餐饮', name: '餐饮'},
+    {iconName: '衣服', name: '衣服'},
+    {iconName: '住房', name: '住房'},
+    {iconName: '出行', name: '出行'},
+    {iconName: '酒店', name: '酒店'},
+    {iconName: '红包', name: '红包'},
+    {iconName: '转账', name: '转账'},
+    {iconName: '商店', name: '商店'},
+    {iconName: '旅行', name: '旅行'},
+    {iconName: '娱乐', name: '娱乐'},
+    {iconName: '其它', name: '其它'},
+  ])
+  const addTag = () => {
+    const tagName = window.prompt('请输入新标签名:')
+    if (tagName) {
+      const oldTags = tags.map(tag => tag.name)
+      if (oldTags.indexOf(tagName) >= 0) {
+        return window.alert('该标签已存在')
+      }
+      setTags([...tags, {iconName: '其它', name: tagName}])
+    }
+  }
   return (
     <Wrapper>
       <Output/>
       <ol>
         {tags.map(tag =>
-          <li key={tag}>
-            <span className="icons"><Icon name={tag}/></span>
-            <span>{tag}</span>
+          <li key={tag.name}>
+            <span className="icons"><Icon name={tag.iconName}/></span>
+            <span>{tag.name}</span>
           </li>
         )}
+        <li>
+          <span className="icons" onClick={addTag}><Icon name="添加"/></span>
+          <span>添加</span>
+        </li>
       </ol>
     </Wrapper>
   )
