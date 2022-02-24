ac292ae879f06f64252e30f1c8be0d69db8cdcbe
Yandhi <15877410869@163.com>
2022-02-24 23:50:05 +0800

    feat: useParams

--- src/views/TagEdit.tsx
+++ src/views/TagEdit.tsx
@@ -1,6 +1,16 @@
-const TagEdit = () => {
+import React from 'react'
+import {useParams} from 'react-router-dom'
+import {useTags} from '../hooks/useTags'
+
+type Params = {
+  id: string
+}
+const TagEdit: React.FC = () => {
+  const {tags} = useTags()
+  const {id} = useParams<Params>()
+  const tag = tags.filter(tag => tag.id === parseInt(id))[0]
   return (
-    <h1>hi</h1>
+    <h1>{tag.name}</h1>
   )
 }
 
