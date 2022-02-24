e1efdd5addc79b87f3d7536d41e13dab78c4c8f1
Yandhi <15877410869@163.com>
2022-02-25 02:56:35 +0800

    feat: TagEdit.tsx style

--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,27 +1,10 @@
-import React, {useRef} from 'react'
+import React, {ChangeEventHandler} from 'react'
 import styled from 'styled-components'
+import {Input} from 'components/Input'
 
 const Wrapper = styled.section`
   padding: 0 16px;
   font-size: 16px;
-
-  > label {
-    display: flex;
-    align-items: center;
-
-    > span {
-      color: #576b95;
-      margin-right: 16px;
-      white-space: nowrap;
-    }
-
-    > input {
-      display: block;
-      width: 100%;
-      height: 60px;
-      border: none;
-    }
-  }
 `;
 
 type Props = {
@@ -29,24 +12,17 @@ type Props = {
   onChange: (note: string) => void
 }
 const NoteSection: React.FC<Props> = (props) => {
-  const note = props.value
-  const refInput = useRef<HTMLInputElement>(null)
-  const changValue = () => {
-    if (refInput.current !== null) {
-      props.onChange(refInput.current.value)
-    }
-  }
+  const note = props.value;
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    props.onChange(e.target.value);
+  };
   return (
     <Wrapper>
-      <label>
-        <span>添加备注</span>
-        <input type="text"
-               placeholder="请输入备注"
-               ref={refInput}
-               defaultValue={note}
-               onBlur={changValue}
-        />
-      </label>
+      <Input label="备注"
+             type="text"
+             value={note}
+             onChange={onChange}
+             placeholder="请填写备注"/>
     </Wrapper>
   )
 }
--- src/views/TagEdit.tsx
+++ src/views/TagEdit.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
+import {useTags} from 'hooks/useTags'
 import {useParams} from 'react-router-dom'
-import {useTags} from '../hooks/useTags'
-import Layout from '../components/Layout'
+import Layout from 'components/Layout'
+import Icon from 'components/Icon'
+import {Input} from 'components/Input'
+import {Space} from 'components/Space'
+import {Center} from 'components/Center'
 
+const TopBar = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  line-height: 20px;
+  padding: 20px;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+`;
+const InputWrapper = styled.div`
+  padding: 0 16px;
+  margin-top: 8px;
+`;
+const Button = styled.button`
+  font-size: 18px;
+  border: none;
+  padding: 8px 12px;
+  background: red;
+  border-radius: 4px;
+  color: white;
+`;
 type Params = {
   id: string
 }
@@ -12,7 +37,23 @@ const TagEdit: React.FC = () => {
   const tag = findTag(parseInt(id))
   return (
     <Layout>
-      <h1>{tag.name}</h1>
+      <TopBar>
+        <Icon name="left"/>
+        <span>编辑标签</span>
+        <Icon/>
+      </TopBar>
+      <InputWrapper>
+        <Input label="标签名"
+               type="text"
+               placeholder="标签名"
+               value={tag.name}/>
+      </InputWrapper>
+      <Center>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Button>删除标签</Button>
+      </Center>
     </Layout>
   )
 }
